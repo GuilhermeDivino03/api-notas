fd9c8fbbc0f371e8a2bfe9cdc4961fc004328fb4
GuilhermeDivino03 <guilhermedivino2003@gmail.com>
2024-05-24 16:05:06 -0300

    Logs adicionado na aplicação

--- src/main/java/com/domain/Notas/controller/NotaController.java
+++ src/main/java/com/domain/Notas/controller/NotaController.java
@@ -2,6 +2,8 @@ package com.domain.Notas.controller;
 
 import com.domain.Notas.model.NotaModel;
 import com.domain.Notas.service.NotaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,37 +13,51 @@ import java.util.List;
 @RestController
 @RequestMapping("/nota")
 public class NotaController {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotaController.class);
     @Autowired
     private NotaService notaService;
 
     @GetMapping("/lista")
     public List<NotaModel> getNotas(){
+        logger.debug("Exibindo lista de notas");
         return notaService.getNotas();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<NotaModel> getnota(@PathVariable Long id){
+    public ResponseEntity<NotaModel> getnota(@PathVariable Long id) {
+        logger.debug("exibindo a nota com id {}", id);
         return notaService.getNota(id)
                 .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+                .orElseGet(() -> {
+                    logger.warn("Nota com id {} não encontrada", id);
+                    return ResponseEntity.notFound().build();
+                });
     }
 
     @PostMapping
     public NotaModel addNota(@RequestBody NotaModel novaNota){
+        logger.debug("Adicionando nova nota: {}", novaNota);
         return notaService.addNota(novaNota);
     }
 
     @PutMapping("/atualizar-nota/{id}")
     public ResponseEntity<NotaModel> updateNota(@PathVariable Long id, @RequestBody NotaModel notaAtualizada){
+        logger.debug("Atualizando a nota com id {}", id);
         return notaService.updateNota(id, notaAtualizada)
                 .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+                .orElseGet(() -> {
+                    logger.warn("Nota com id {} não encontrada para atualização", id);
+                    return ResponseEntity.notFound().build();
+                });
     }
 
     public ResponseEntity<Void> deleteNota(@PathVariable Long id){
+        logger.debug("Deletando nota com id {}", id);
         if (notaService.deleteNota(id)) {
             return ResponseEntity.ok().build();
         } else
+            logger.warn("Nota com id {} não encontrada para deleção", id);
             return ResponseEntity.notFound().build();
     }
 
--- src/main/java/com/domain/Notas/service/NotaService.java
+++ src/main/java/com/domain/Notas/service/NotaService.java
@@ -1,7 +1,10 @@
 package com.domain.Notas.service;
 
+import com.domain.Notas.controller.NotaController;
 import com.domain.Notas.model.NotaModel;
 import com.domain.Notas.repository.NotaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,34 +13,45 @@ import java.util.Optional;
 
 @Service
 public class NotaService {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotaController.class);
+
     @Autowired
     private NotaRepository notaRepository;
 
     public List<NotaModel> getNotas() {
+        logger.debug("Buscando todas as notas");
         return notaRepository.findAll();
     }
 
     public Optional<NotaModel> getNota(Long id){
+        logger.debug("Buscando nota com id {}", id);
         return notaRepository.findById(id);
     }
 
     public NotaModel addNota(NotaModel novaNota){
+        logger.debug("Salvando nova nota: {}", novaNota);
         return notaRepository.save(novaNota);
     }
 
     public Optional<NotaModel> updateNota(Long id, NotaModel notaAtualizada){
+        logger.debug("Atualizando nota com id {}", id);
         return notaRepository.findById(id).map(notaModel -> {
             notaModel.setTitulo(notaAtualizada.getTitulo());
             notaModel.setConteudo(notaAtualizada.getConteudo());
+            logger.debug("Nota atualizada: {}", notaAtualizada);
             return notaRepository.save(notaModel);
         });
     }
 
     public boolean deleteNota(Long id){
+        logger.debug("Deletando nota com id {}", id);
         if (notaRepository.existsById(id)) {
             notaRepository.deleteById(id);
+            logger.debug("Nota deletada com sucesso");
             return true;
         }
+            logger.warn("Nota com id {} não encontrada para deleção", id);
             return false;
     }
 }
