b9dcc57c3e46bbd4c0cc711ee7b9df8c342f1dec
GuilhermeDivino03 <guilhermedivino2003@gmail.com>
2024-05-27 12:01:07 -0300

    Adicionando swagger a aplicação

--- src/main/java/com/domain/Notas/controller/NotaController.java
+++ src/main/java/com/domain/Notas/controller/NotaController.java
@@ -12,18 +12,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/nota")
+//@Api(value = "Notas API", tags = { "Notas" })
 public class NotaController {
 
     private static final Logger logger = LoggerFactory.getLogger(NotaController.class);
     @Autowired
     private NotaService notaService;
 
+    //@ApiOperation(value = "Obtém todas as notas", response = List.class)
     @GetMapping("/lista")
     public List<NotaModel> getNotas(){
         logger.debug("Exibindo lista de notas");
         return notaService.getNotas();
     }
 
+    //@ApiOperation(value = "Obtém uma nota pelo ID", response = NotaModel.class)
     @GetMapping("/{id}")
     public ResponseEntity<NotaModel> getnota(@PathVariable Long id) {
         logger.debug("exibindo a nota com id {}", id);
@@ -35,12 +38,14 @@ public class NotaController {
                 });
     }
 
+    //@ApiOperation(value = "Adiciona uma nova nota", response = NotaModel.class)
     @PostMapping
     public NotaModel addNota(@RequestBody NotaModel novaNota){
         logger.debug("Adicionando nova nota: {}", novaNota);
         return notaService.addNota(novaNota);
     }
 
+    //@ApiOperation(value = "Atualiza uma nota existente", response = NotaModel.class)
     @PutMapping("/atualizar-nota/{id}")
     public ResponseEntity<NotaModel> updateNota(@PathVariable Long id, @RequestBody NotaModel notaAtualizada){
         logger.debug("Atualizando a nota com id {}", id);
@@ -52,6 +57,8 @@ public class NotaController {
                 });
     }
 
+   // @ApiOperation(value = "Deleta uma nota existente", response = Void.class)
+    @DeleteMapping
     public ResponseEntity<Void> deleteNota(@PathVariable Long id){
         logger.debug("Deletando nota com id {}", id);
         if (notaService.deleteNota(id)) {
